86d57768a3574e2bf79bf85458a89f0cd37265d1
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-01-27 21:55:25 +0900

    ㅠ

---
+++ 긴급/main.py
@@ -0,0 +1,33 @@
+from abc import ABC,abstractmethod
+
+class Serve(ABC):
+    
+    def create(self):
+        self.add_money()
+        self.product()
+        self.action()
+        
+    def add_money(self):
+        user_money = int(input("돈을 넣어주세요"))
+        return user_money
+    
+    def money_valid(self):
+        if self.add_money < self.product:
+            print("금액이 부족합니다")
+        else:
+            return self.add_money
+
+    @abstractmethod
+    def product(self):
+        pass
+    @abstractmethod
+    def action(self):
+        pass
+
+    def play(self):
+        print(f"돈 투입 {self.add_money}")
+        money = self.money_valid()
+        print(self.action)
+        print("제품배출 완료")
+        print({self.add_money-self.product})
+        
